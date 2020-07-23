e7531c1dbbb9a17f59536f56bd92ccfe6a714297
dhk5844 <dhk5844@ajou.ac.kr>
2020-07-23 12:52:27 +0900

    Complete CalculatorTest

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -1,8 +1,11 @@
 
-import org.junit.Before;
+
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.*;
 
 public class CustomCalculatorTest {
@@ -11,37 +14,104 @@ public class CustomCalculatorTest {
 
     //더하기 테스트 작성
     @Test
-    public void add() {
+    public void addTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertTrue(result==25);
+        assertThat(result,is(25));
+        System.out.println("result :: " + result);
+    }
+    //더하기 false 테스트
+    @Test
+    public void addFalseTest(){
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.add(10,15);
+
+        assertFalse(result == 25);
         System.out.println("result :: " + result);
     }
 
     //빼기 테스트 작성
     @Test
-    public void subtract() {
+    public void subtractTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertTrue(result==13);
         System.out.println("result :: " + result);
+    }
+    //빼기 false 테스트
+    @Test
+    public void subtractFalseTest(){
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.subtract(23,10);
+        assertFalse(result == 12);
     }
 
     //곱하기 테스트 작성
     @Test
-    public void multiply() {
+    public void multiplyTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertThat(result, is(45));
+        System.out.println("result :: " + result);
+    }
+    //곱하기 false 테스트
+    @Test
+    public void multiplyFalseTest(){
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.multiply(5,9);
+
+        assertFalse(result == 12);
         System.out.println("result :: " + result);
     }
 
     //나누기 테스트 작성
     @Test
-    public void divide() {
+    public void divideTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertThat(result, is(5));
         System.out.println("result :: " + result);
+    }
+    //나누기 flase 테스트
+    @Test
+    public void divideFalseTest() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.divide(25,5);
+
+        assertFalse(result==5);
+        System.out.println("result :: " + result);
+    }
+    //시간제한 실패 test
+    @Test(timeout = 4000)
+    public void timeInMethodTest() throws InterruptedException{
+        Thread.sleep(5000);
+    }
+    //시간제한 성공 test
+    @Test(timeout = 3000)
+    public void 시간제한체크() throws InterruptedException{
+        Thread.sleep(2000);
+    }
+    //exception이 기대하는 것이 맞는지 test
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIsEmptyindexOutOfBoundException(){
+        new ArrayList<Object>().get(0);
+    }
+
+
+    @Test
+    public void testMethod(){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        assertThat(myList, is(empty()));
+    }
+
+    @Test
+    public void isEmptyArray(){
+        ArrayList<Object> myList = new ArrayList<>();
+        assertThat(myList, is(empty()));
     }
 }
